cbbd42cfe4ac9fb50670cb24feb05f6928845129
Júlia Botelho <cajujumo@gmail.com>
2023-01-26 11:03:00 -0300

    feat: complete pair programming practice - carsPrisma

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,55 @@
+import { number } from "joi";
+import prisma from "../config/database.js";
+
+
+async function getCars() {
+  return await prisma.cars.findMany();
+}
+
+async function getCar(id: number) {
+
+  return await prisma.cars.findUnique({
+    where: {
+      id: id
+    }
+  })
+
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+
+  return await prisma.cars.findUnique({
+    where: {
+      licensePlate: licensePlate
+    }
+  })
+}
+
+async function createCar(model: string, licensePlate: string, year: string, color: string) {
+
+  await prisma.cars.create({
+    data: {
+      "model": model,
+      "licensePlate": licensePlate,
+      "year": year,
+      "color": color
+    }
+  });
+}
+
+async function deleteCar(id: number) {
+
+  await prisma.cars.delete({where:{
+    id:id
+  }})
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
